a660dea2e8801a9f87793d5f9b3d0cfca549f667
anh <dottudaudenvi@gmail.com>
2023-12-14 11:26:58 +0700

    comit page1

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+const app = express()
+require("dotenv").config()
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+
+app.listen(port, (req, res) =>{
+    console.log(`Server running on port...: ${port}`);
+});
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=> console.log("MongooDB connection estalished"))
+.catch((error) => console.log("MongooDB connection failed: ", error.message));
